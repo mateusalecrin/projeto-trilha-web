b3b16caaaeb07a254cfa5dceb52626181cbb7a39
MATEUS BRUNO DE ALECRIN <mateus_alecrin@estudante.sesisenai.org.br>
2023-05-17 21:58:36 -0300

    feat: configuring rest

---
+++ ProjetoTrilhaWeb/WebContent/js/product.js
@@ -0,0 +1,18 @@
+COLDIGO.produto = new Object();
+
+$(document).ready(function() {
+	
+	// Carrega as marcas registradas no BD no select do formulário de inserir
+	COLDIGO.produto.carregarMarcas = function() {
+		$.ajax({
+			type: "GET",
+			url: "/ProjetoTrilhaWeb/rest/marca/buscar",
+			sucess: function() {
+				
+			},
+			error: function() {
+				
+			}
+		});
+	}
+});
